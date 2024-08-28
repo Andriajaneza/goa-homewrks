f303d62d7ec71f533e24f76cf3f8478f10f8893d
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:59:46 -0700

    goa

---
+++ day 14/classwork/homework.py
@@ -0,0 +1,19 @@
+#1
+
+print(True or False)
+print(True or True)
+print(False or False)
+
+print(False and True)
+print(False and False)
+print(True and True)
+
+#2
+
+is_true = True
+temp = 34 > 32
+
+print(is_true and temp)
+
+#3
+
