fe77656e6174db83e3c55fb0da8f6707e62dd695
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:43:55 -0700

    goa

---
+++ day 02/homework/homework.py
@@ -0,0 +1,41 @@
+
+#mother
+
+mother = "shorena"
+print ("my mother is " + (mother))
+
+#father
+
+father = "irakli"
+print ("my father is " + (father))
+
+#my brother
+
+brother = "robi"
+print ("my brother is " + (brother))
+
+
+#emotion
+
+emotion = "goa is very nice academy where you can learn programing"
+print  ("my emotions as goa is - " + (emotion))
+
+
+#mathematycs
+
+num1 = "1"
+num2 = "2"
+num3 = "3"
+num4 = "4"
+
+print ((num1) + " + " + (num2) + " = " + (num3))
+
+print ((num2) + " + " + (num2) + " = " + (num4))
+
+print ((num3) + " - " + (num1) + " = " + (num2))
+
+print ((num4) + " - " + (num3) + " = " + (num1))
+
+robi = "siri"
+
+print (robi)
