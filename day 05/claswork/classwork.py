2d0ddf2d9bdadee4f2c6d6f53956de702b2533cf
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:49:59 -0700

    goa

---
+++ day 05/claswork/classwork.py
@@ -0,0 +1,17 @@
+name = "andria"
+print (name)
+
+my_name = "andria"
+print (my_name)
+
+name = "andria"
+print (name)
+
+first_number=10
+second_number=5
+sum = first_number + second_number
+print(sum)
+
+სახელი = "andria"
+გვარი = "janezashvili"
+print (სახელი + გვარი)
---
+++ day 05/homework/homework.py
@@ -0,0 +1,39 @@
+num1 = 20
+
+num2 = 30
+
+total = num1 + num2
+
+print (total)
+
+greeting = "hello"
+age = 25
+message = (greeting + ", i am " + str(age) + " years old.")
+print(message)
+
+
+a = 15
+b = 5
+result = a * b
+print ("result of division is: " + str(result))
+
+
+first_name = "john"
+last_name = "doe"
+print (first_name + " " + last_name)
+
+greeting = "hello"
+name = "world"
+full_message = greeting + "" + name
+print (full_message)
+
+a = 50
+b = 100
+result = a+b
+print (result)
+
+man = "lazy"
+bananas = 5
+print (man + " has " + str(bananas) + " bananas.")
+
+
