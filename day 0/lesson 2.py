2fa157877d0a0bea1f87fcb187ee1983240eddab
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:41:01 -0700

    goa

---
+++ day 0/lesson 2.py
@@ -0,0 +1,12 @@
+name = "andria"
+surname = "janezashvili"
+heigh = "135cm"
+age = "11"
+gender = "male"
+
+
+print (name + " " + surname)
+print ("age "+ age)
+print ("heigh " + heigh)
+print ("gender " + gender)
+print ("he is beautifull")
