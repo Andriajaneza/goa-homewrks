8eb157c9febedb64ffde8ffa0969870d535b3956
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:51:47 -0700

    goa

---
+++ day 07/classwork/classwork.py
@@ -0,0 +1,10 @@
+best_food=input("which food do you like?")
+print(best_food)
+
+name_and_surname = input("andria janezashvili")
+print (name_and_surname)
+
+age=input(11)
+print(11 + 10)
+
+concatation = input("concatation is string + string")
---
+++ day 07/homework/homework.py
@@ -0,0 +1,12 @@
+#input არის ფუნქცია output არის რაც გარედან ჩანს.
+
+#კონკატენცია არის string + string.
+
+name = input("name :")
+surname = input("surname :")
+
+print (name + " " + surname)
+
+eating_what_i_want = input("which food do you want? :")
+print(eating_what_i_want)
+
