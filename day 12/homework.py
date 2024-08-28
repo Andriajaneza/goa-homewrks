70d869b5de6c25a25a052b9164f332eac4f8600c
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:58:20 -0700

    goa

---
+++ day 12/homework.py
@@ -0,0 +1,9 @@
+coca_cola_value = int(input("coca cola value: "))
+bebos_kampoti = int(input("bebos kampoti: "))
+print(coca_cola_value < bebos_kampoti)
+
+camel1 = 5<3
+camel2 = 3<5
+
+print(camel1)
+print(camel2)
