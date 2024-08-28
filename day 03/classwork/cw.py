8bbf1eee1e5c65e4ccbae1fd9fdff7b101a09d8f
Andriajaneza <andriajazneza@gmail.com>
2024-08-28 04:46:33 -0700

    goa

---
+++ day 03/classwork/cw.py
@@ -0,0 +1,12 @@
+
+myage=(11)
+myageintenyears=(21)
+
+print(myage)
+print(myageintenyears)
+
+goa="GOA "
+print (goa * 11111111111111111)
+
+
+
---
+++ day 03/homework/homework.py
@@ -0,0 +1,11 @@
+text = "GOA "
+print ((text) * 1000)
+
+
+year = "11"
+print ("i am " + (year) + " years old")
+
+monkey = (20)
+monkey_banana = (5)
+print (monkey - monkey_banana)
+
