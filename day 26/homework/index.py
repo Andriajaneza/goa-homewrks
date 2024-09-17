564445f512ad1f6c1b8ef7f1109476a21057e546
Andriajaneza <andriajazneza@gmail.com>
2024-09-17 06:53:54 -0700

    goa

---
+++ day 26/homework/index.py
@@ -0,0 +1,14 @@
+students = ["ანდრია","ჯანეზაშვილი","11","|","ფილიპე","ცხადაია","11","|","კესანე","ბრეგვაძე","7","|","..."]
+print (students)
+
+menu = ["ხინკალი","1.20 L","ხაჭაპური","8 L","მწვადი","3 L","ყავა","2 L","წყალი","FREE","ქაბაბი","5 L"]
+print (menu)
+
+movies = ["harry potter","10|9","fall","10|8.5","dr.strange","10|8.5","hotel transilvania","10|7.5","titanic","10|9.5",]
+print (movies)
+
+family = ["mother","shorena gurgenashvili 5793369**","father","irakli janezashvili 5715012**","brother","robi janezashvili 5989087**",]
+print (family)
+
+random_theme = ["კაცის","კაცის","კაცურო","კაცმაცურო","ცანცურო","წავედით","მამაცო"]
+print (random_theme)
