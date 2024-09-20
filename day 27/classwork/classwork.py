a73d0bf358f0ba481a792affeea407045285888c
Andriajaneza <andriajazneza@gmail.com>
2024-09-20 07:48:59 -0700

    goa

---
+++ day 27/classwork/classwork.py
@@ -0,0 +1,11 @@
+#1
+product_list = ["chips","apple","chesse","cookie","festables bar","cola"]
+print(product_list)
+
+#2
+product_list = ["chips","apple","chesse","cookie","festables bar","cola"]
+print(product_list[-6:])
+
+#3
+product_list = ("chips","apple","chesse","cookie","festables bar","cola")
+print(product_list[::-1])
